8f4d5dd558604f5060d0edc04efcfd0d604d714e
Francois Visconte <f.visconte@criteo.com>
2014-11-03 15:40:28 +0100

    Remove ldap_plugin recipe
    Install plugin only need a remote_file into the right folder.
    There is no reason to create a recipe to install the ldap plugin only.

--- attributes/ldap_plugin.rb
+++
@@ -1,6 +0,0 @@
-
-
-default['sonar']['ldap_auth']['plugin_url'] = 'http://repository.codehaus.org/org/codehaus/sonar-plugins/sonar-ldap-plugin/1.4/sonar-ldap-plugin-1.4.jar'
-default['sonar']['ldap_auth']['plugin_checksum'] = '65dc397094a534b41365b96770906ba2e99b36cd98c924728f4b1dcfcac3010a'
-
-
--- recipes/ldap_plugin.rb
+++
@@ -1,8 +0,0 @@
-
-
-remote_file '/opt/sonar/extensions/sonar-ldap-plugin.jar' do
-  source node['sonar']['ldap_auth']['plugin_url']
-  checksum node['sonar']['ldap_auth']['plugin_checksum']
-  mode 0644
-end
-
